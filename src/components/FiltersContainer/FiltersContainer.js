e30353cece9258f68b34ac3324c3084e096b76b0
Natalka2019 <nrushchak.gmail.com>
2021-06-09 16:27:57 +0300

    add select to filters container

--- src/components/FiltersContainer/FiltersContainer.js
+++ src/components/FiltersContainer/FiltersContainer.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import styles from './FiltersContainer.module.scss';
 import Button from '../Button';
+import SelectField from '../SelectField';
+
+const options = [
+  { id: 1, value: 'one', label: 'One' },
+  { id: 2, value: 'two', label: 'Two' },
+  { id: 3, value: 'three', label: 'Threeeeeeeeeeeeeeeeeeeee' }
+];
+
+const initialValue = options[2];
 
 function FiltersContainer() {
+  const onChange = (selectedOption) => {
+    console.log(selectedOption);
+  };
+
   return (
     <div className = {styles.container}>
       <Button />
+      <SelectField
+        initialValue = {initialValue}
+        options = {options}
+        label = 'Count'
+        onChange = {onChange}
+      />
     </div>
   );
 }
--- src/pages/Discounts/Discounts.js
+++ src/pages/Discounts/Discounts.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Discounts.module.scss';
+import FiltersContainer from '../../components/FiltersContainer';
 
 function Discounts() {
   return (
     <div className = {styles.container}>
       <h1>Discounts page</h1>
+      <FiltersContainer />
     </div>
   );
 }
