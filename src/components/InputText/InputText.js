6fdb6ba3e8c6c6b2ec2caf190c6e5b518549a1e2
Roma <romaqwer08@gmail.com>
2021-06-09 21:37:38 +0300

    add input critical attributes

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -2,20 +2,25 @@ import styles from './InputText.module.scss';
 
 export default function InputText(
   {
+    label,
     className = '',
     onInput,
     placeholder,
-    label
+    name,
+    value = ''
   }
 ) {
   return (
-    <div className={ styles.wrap }>
-      { label != null && <label>{ label }</label> }
+    <div className={styles.wrap}>
+      {label != null && <label htmlFor={name}>{ label }</label>}
       <input
-        className={ `${styles.input} ${styles[className]}` }
+        className={`${styles.input} ${styles[className]}`}
         type='text'
-        placeholder={ placeholder }
-        onInput={ onInput }
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        onInput={onInput}
+        value={value}
       />
     </div>
   );
