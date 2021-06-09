575ec49dbad3be9d448f5c61b7242e5293896ee0
Natalka2019 <nrushchak.gmail.com>
2021-06-09 14:10:01 +0300

    add filtersContainer

---
+++ src/components/FiltersContainer/FiltersContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './FiltersContainer.module.scss';
+import Button from '../Button';
+
+function FiltersContainer() {
+  return (
+    <div className = {styles.container}>
+      <Button />
+    </div>
+  );
+}
+
+export default FiltersContainer;
