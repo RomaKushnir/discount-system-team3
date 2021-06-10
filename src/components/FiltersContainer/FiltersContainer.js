b3bd25f244f150cd74ebd89c8685a66050fefdb2
Natalka2019 <nrushchak.gmail.com>
2021-06-10 14:11:43 +0300

    add mock data to filter

--- src/components/FiltersContainer/FiltersContainer.js
+++ src/components/FiltersContainer/FiltersContainer.js
@@ -1,30 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FiltersContainer.module.scss';
 import Button from '../Button';
 import SelectField from '../SelectField';
 
-const options = [
-  { id: 1, value: 'one', label: 'One' },
-  { id: 2, value: 'two', label: 'Two' },
-  { id: 3, value: 'three', label: 'Threeeeeeeeeeeeeeeeeeeee' }
-];
+function FiltersContainer({
+  onApplyButtonClick,
+  countriesList,
+  citiesList,
+  categoriesList,
+  vendorsList
+}) {
+  const [searchWord, setSearchWord] = useState('');
+  const [countries, setCountries] = useState(countriesList[0]);
+  const [cities, setCities] = useState(citiesList[0]);
+  const [categories, setCategories] = useState(categoriesList[0]);
+  const [vendor, setVendor] = useState(vendorsList[0]);
 
-const initialValue = options[2];
+  const onChangeInput = (e) => {
+    console.log(e.target.value);
+    setSearchWord(e.target.value);
+  };
 
-function FiltersContainer() {
-  const onChange = (selectedOption) => {
+  const onChangeCountries = (selectedOption) => {
     console.log(selectedOption);
+    setCountries(selectedOption);
+  };
+
+  const onChangeCities = (selectedOption) => {
+    console.log(selectedOption);
+    setCities(selectedOption);
+  };
+
+  const onChangeCategories = (selectedOption) => {
+    console.log(selectedOption);
+    setCategories(selectedOption);
+  };
+
+  const onChangeVendor = (selectedOption) => {
+    console.log(selectedOption);
+    setVendor(selectedOption);
   };
 
   return (
     <div className = {styles.container}>
-      <Button />
-      <SelectField
-        initialValue = {initialValue}
-        options = {options}
-        label = 'Count'
-        onChange = {onChange}
+      <div className = {styles.filtersContainer}>
+        <div className = {styles.smallColumn}>
+          <SelectField
+            initialValue = {countriesList[0]}
+            options = {countriesList}
+            label = 'Country'
+            onChange = {onChangeCountries}
+          />
+          <SelectField
+            initialValue = {citiesList[0]}
+            options = {citiesList}
+            label = 'City'
+            onChange = {onChangeCities}
+          />
+        </div>
+        <div className = {styles.smallColumn}>
+            <SelectField
+              initialValue = {categoriesList[0]}
+              options = {categoriesList}
+              label = 'Category'
+              onChange = {onChangeCategories}
+            />
+            <SelectField
+              initialValue = {vendorsList[0]}
+              options = {vendorsList}
+              label = 'Vendor'
+              onChange = {onChangeVendor}
+            />
+        </div>
+      </div>
+      <div className = {styles.inputContainer}>
+        <input onChange = {onChangeInput}/>
+      </div>
+      <div className = {styles.buttonContainer}>
+      <Button
+        btnText = 'Apply'
+        onClick = {() => onApplyButtonClick(
+          {
+            searchWord,
+            countries,
+            cities,
+            categories,
+            vendor
+          }
+        )}
       />
+      </div>
     </div>
   );
 }
