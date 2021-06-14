1a69c69aacb9e4aca42b42980a59e66153421220
Kyrylo_Korotych <mshelper757@gmail.com>
2021-06-14 11:53:42 +0300

    ResolveCard

--- src/components/discountCard/DiscountCard.js
+++ src/components/discountCard/DiscountCard.js
@@ -9,20 +9,24 @@ function DiscountCard({
   img = 'https://picsum.photos/200/300',
   description,
   discount
-}) { // export default нужен для того, чтобы экспортировать в другом файле
+}) {
   return (
         <div className={style.borderCard}>
         <div className={style.col1}>
-          <img src={img} alt={'vendor'} width="90" height="90"/>
+          <img src={img} alt={'vendor'} width='90' height='90'/>
         </div>
-          <div className={style.col2}>
-          <div className={style.row}> <h2>{title}</h2> </div>
+        <div className={style.col2}>
+          <div className={style.row}><h2>{title}</h2></div>
         <div className={style.row}>
-          <p className={style.col3}>{category}</p>
-          <p className={style.col3}>{company}</p>
+          <div className={style.col3}><p>{category}</p></div>
+          <div className={style.col3}><p>{company}</p></div>
         </div>
-          <div className={style.row}> <p className={style.description}>{description}</p> </div>
-          <div className={style.row}> <p className={style.disc}>{discount}</p> </div>
+        <div className={style.row}>
+          <p className={style.description}>{description}</p>
+        </div>
+        <div className={style.row}>
+          <p className={style.disc}>{discount}</p>
+        </div>
         </div>
         </div>
   );
