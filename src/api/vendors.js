63e1cf1ca72c3a1c22d1b7f92d693cbf6022b750
Roma <romaqwer08@gmail.com>
2021-06-16 23:19:09 +0300

    implement get vendors from api, delete vendor

--- src/api/vendors.js
+++ src/api/vendors.js
@@ -1,5 +1,4 @@
-// import axios from './axiosClient';
-import vendorsList from '../mockData/VendorsListPage';
+import axios from './axiosClient';
 
 export const addVendor = (data) => {
   console.log(data); // temporary to check flow
@@ -7,21 +6,11 @@ export const addVendor = (data) => {
   return 'Vendor successfully added'; // temporary to check flow
 };
 
-export const updateVendor = (data) => {
-  console.log(data); // temporary to check flow
+export const updateVendor = (data) => axios.put(`/vendors/${data.id}`, {});
 
-  return 'Vendor successfully updated'; // temporary to check flow
-};
-
-export const deleteVendor = (id) => {
-  console.log(id); // temporary to check flow
-
-  return 'Vendor successfully deleted'; // temporary to check flow
-};
+export const deleteVendor = (id) => axios.delete(`/vendors/${id}`);
 
 export const getVendors = () => {
   console.log('Get Vendors!'); // temporary to check flow
-  console.log('getVendors AXIOS', Date.now());
-  // return axios.get('vendors');
-  return vendorsList; // temporary to check flow
+  return axios.get('/vendors');
 };
--- src/store/reducers/vendorReducer.js
+++ src/store/reducers/vendorReducer.js
@@ -10,7 +10,21 @@ const vendorReducer = (state = initialState, action) => {
       const { payload } = action;
       return {
         ...state,
+        vendors: payload.data
+      };
+    }
+    case types.UPDATE_VENDOR_SUCCESS: {
+      const { payload } = action;
+      return {
+        ...state,
         vendors: payload
+      };
+    }
+    case types.DELETE_VENDOR_SUCCESS: {
+      const { payload } = action;
+      return {
+        ...state,
+        vendors: state.vendors.filter((el) => el.id !== payload)
       };
     }
     default:
