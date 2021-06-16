d6e1c53b2c872628989edca99e4bc2a3df850a6c
AlionaN <alionanovyk99@gmail.com>
2021-06-16 11:32:03 +0300

    feat: add mock data for discounts

---
+++ src/mockData/discountsList.js
@@ -0,0 +1,22 @@
+const discountsList = [
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  },
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  },
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  },
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  },
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  },
+  {
+    categoryId: 0, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", expirationDate: "2021-07-30T23:59:59", flatAmount: 50, id: 0, imageUrl: '', perUser: 1, percentage: 20, price: 200, quantity: 50, shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit", startDate: "2021-06-16T00:00:00", title: "Title 1", vendorId: 3
+  }
+];
+
+export default discountsList;
