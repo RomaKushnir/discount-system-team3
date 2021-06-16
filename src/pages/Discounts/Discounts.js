51cd9d33f4639b31718c206d9a6176c7874798d8
AlionaN <alionanovyk99@gmail.com>
2021-06-16 11:32:48 +0300

    feat: add header and footer on Discounts page

--- src/pages/Discounts/Discounts.js
+++ src/pages/Discounts/Discounts.js
@@ -5,6 +5,16 @@ import countriesList from '../../mockData/countriesList';
 import citiesList from '../../mockData/citiesList';
 import categoriesList from '../../mockData/categoriesList';
 import vendorsList from '../../mockData/vendorsList';
+import Header from '../../components/Header';
+import Routes from '../../routes';
+import Footer from '../../components/Footer';
+
+const navLinks = {
+  categories: Routes.CATEGORIES,
+  main: Routes.ROOT,
+  vendors: Routes.VENDORS,
+  discounts: Routes.DISCOUNTS
+};
 
 function Discounts() {
   const onApplyButtonClick = (parameters) => {
@@ -12,15 +22,26 @@ function Discounts() {
   };
 
   return (
-    <div className = {styles.container}>
-      <h1>Discounts page</h1>
+    <div className = {styles.containerFluid}>
+      <div className = {styles.container}>
+      <Header
+        mainLink = {navLinks.main}
+        categoriesLink = {navLinks.categories}
+        vendorsLink = {navLinks.vendors}
+        discountsLink = {navLinks.discounts}
+      />
+      </div>
+      <div className = {styles.container}>
       <FiltersContainer
         onApplyButtonClick = {onApplyButtonClick}
         countriesList = {countriesList}
         citiesList = {citiesList}
         categoriesList = {categoriesList}
         vendorsList = {vendorsList}
+        className = {styles.discountsFilter}
       />
+      </div>
+      <Footer/>
     </div>
   );
 }
