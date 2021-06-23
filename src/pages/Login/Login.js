932335b5946d691ef82124e9fd12fb5174dfb1d1
Natalka2019 <nrushchak.gmail.com>
2021-06-23 16:48:25 +0300

    add eye icon to password field

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,69 @@
+import { useState } from 'react';
 import styles from './Login.module.scss';
 import TextInput from '../../components/TextInput';
 import Button from '../../components/Button';
+import PasswordField from './components/PasswordField';
+
+const inputStyles = {
+  width: '330px',
+  height: '45px'
+};
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
+  const onLoginButtonClick = () => {
+    console.log(email, password);
+  };
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const onEyeClick = () => {
+    setPasswordVisibility(!passwordVisibility);
+  };
+
   return (
     <div className = {styles.container}>
+      <div className={styles.logo}>Discount<span className={styles.logoItem}>App</span></div>
       <main className={styles.contentWrapper}>
         <div className = {styles.inputsContainer}>
-          <TextInput/>
-          <TextInput/>
+          <TextInput
+            onValueChange = {onEmailChange}
+            placeholder = "Email"
+            label = "Email"
+            style = {inputStyles}
+            name = "email"
+            type = "email"
+            // onBlur={onBlur}
+            required
+            // touched = {touched.email}
+            // error = {errors.email}
+            />
+          <PasswordField
+            onValueChange = {onPasswordChange}
+            placeholder = "Password"
+            label = "Password"
+            name = "password"
+            type = {passwordVisibility ? 'text' : 'password'}
+            onIconClick = {onEyeClick}
+            passwordVisibility = {passwordVisibility}
+            // onBlur={onBlur}
+            required
+            // touched = {touched.email}
+            // error = {errors.email}
+          />
         </div>
         <div className = {styles.buttonContainer}>
-          <Button />
+          <Button
+            btnText = "Login"
+            onClick = {onLoginButtonClick}
+            // isDisabled = {isDisabled}
+            type = "submit"
+          />
         </div>
       </main>
     </div>
