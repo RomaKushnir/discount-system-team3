e2f11222f0833563da74785ff2944ad674284405
AlionaN <alionanovyk99@gmail.com>
2021-06-25 17:25:58 +0300

    fix: fix bug in conversion date function

--- src/utilities/getMonthAndDay.js
+++ src/utilities/getMonthAndDay.js
@@ -1,9 +1,9 @@
 const DATE_MONTH_LENGTH = 2;
 function getMonthAndDay(date) {
   const dateObj = new Date(date);
-  const day = dateObj.getDate().padStart(DATE_MONTH_LENGTH, '0');
-  const month = dateObj.getMonth().padStart(DATE_MONTH_LENGTH, '0');
-  return `${day}.${month}.`;
+  const day = String(dateObj.getDate());
+  const month = String(dateObj.getMonth());
+  return `${day.padStart(DATE_MONTH_LENGTH, '0')}.${month.padStart(DATE_MONTH_LENGTH, '0')}.`;
 }
 
 export default getMonthAndDay;
