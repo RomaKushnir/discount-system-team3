d1deba44eba94b7c620e11d4ceeddd8d2ed11e90
Natalka2019 <nrushchak.gmail.com>
2021-06-26 07:51:15 +0300

    add console log

--- src/pages/Discounts/components/DiscountList/DiscountList.js
+++ src/pages/Discounts/components/DiscountList/DiscountList.js
@@ -2,7 +2,8 @@ import styles from './DiscountList.module.scss';
 import DiscountCard from '../../../../components/discountCard';
 
 function DiscountList({ discounts }) {
-  const discountsList = discounts.length > 0
+  console.log(discounts);
+  const discountsList = discounts.length
     ? discounts.map((discount) => <li key={discount.id}>
         <DiscountCard
           title = {discount.title}
