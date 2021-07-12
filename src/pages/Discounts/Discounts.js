a90d77f547acc299278abcb70e7d21c9066b022b
AlionaN <alionanovyk99@gmail.com>
2021-07-12 12:28:46 +0300

    feat: implement getDiscountById redux flow

--- src/pages/Discounts/Discounts.js
+++ src/pages/Discounts/Discounts.js
@@ -31,7 +31,6 @@ function Discounts() {
 
   const [modalState, setModalState] = useState(false);
   const [isDiscountModalShown, setIsDiscountModalShown] = useState(false);
-  const [discount, setDiscount] = useState(null);
 
   useEffect(() => {
     dispatch(actions.locationActions.getLocationsList());
@@ -43,6 +42,7 @@ function Discounts() {
 
   const getDiscountsStatus = useSelector((state) => state.discountsReducer.getDiscountsStatus);
   const discountsArray = useSelector(getDiscountsList);
+  const discountById = useSelector((state) => state.discountsReducer.discountById);
   const discountsFilters = useSelector((state) => state.discountsReducer.discountsFilters);
   const discountsFiltersApplied = useSelector((state) => state.discountsReducer.discountsFiltersApplied);
   const user = useSelector((state) => state.userReducer.user);
@@ -105,10 +105,9 @@ function Discounts() {
     }
   };
   const onCardClick = useCallback((e, id) => {
+    dispatch(actions.discountsActions.getDiscountById(id));
     setIsDiscountModalShown(true);
-    const selectedDiscount = discountsArray.find((el) => el.id === id);
-    setDiscount(selectedDiscount);
-  }, [discountsArray]);
+  }, [dispatch]);
 
   const onDiscountModalClose = () => {
     setIsDiscountModalShown(false);
@@ -156,7 +155,7 @@ function Discounts() {
                 favouriteDiscounts = {favourite}
               />
               <DiscountModal
-                discount = {discount}
+                discount = {discountById}
                 isOpen = {isDiscountModalShown}
                 onClose = {onDiscountModalClose}
                 onDeleteDiscount = {onDeleteDiscount}
