adf66336adaee1528cce6c3f72b51d9d4c599583
Natalka2019 <nrushchak.gmail.com>
2021-07-13 11:08:05 +0300

    add locations select

--- src/pages/Vendor/components/VendorInfo/VendorInfo.js
+++ src/pages/Vendor/components/VendorInfo/VendorInfo.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import style from './VendorInfo.module.scss';
+import SelectField from '../../../../components/SelectField';
+import combineLocation from '../../../../utilities/combineLocation';
 
 function VendorInfo({
   vendor
 }) {
   const {
-    title, location, imageUrl, email
+    title, locations, imageUrl, email
   } = vendor;
-  const { countryCode, city, addressLine } = location;
+  const locationOptions = locations.map((location) => combineLocation(location));
+
   return (
     <>
       { vendor && <div className={style.container}>
@@ -17,7 +20,12 @@ function VendorInfo({
       <div className={style.details}>
         <h2>{title}</h2>
         <p>{email}</p>
-        <p>{countryCode}, {city}, {addressLine}</p>
+        <SelectField
+          initialValue = {locationOptions[0]}
+          options = {locationOptions}
+          label = "Locations"
+          isClearable = {false}
+        />
       </div>
       </div>
       }
