6addc59ea67aa10c8e1f2d50fc3687709772ed4b
Natalka2019 <nrushchak.gmail.com>
2021-07-16 08:05:53 +0300

    redirect to previous path

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,15 +1,19 @@
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Routes from '../routes';
 
 function PrivateRoute({ component: Component, ...rest }) {
   const isLoggedIn = useSelector((state) => state.userReducer.loggedIn);
+  const location = useLocation();
+  const currentLocation = location.pathname;
+
+  console.log(currentLocation);
 
   return (
     <Route {...rest} component = {(props) => (
       isLoggedIn
         ? <Component {...props} />
-        : <Redirect to = {Routes.ROOT} />
+        : <Redirect to = {{ pathname: Routes.ROOT, state: { from: currentLocation } }} />
     )}/>
   );
 }
